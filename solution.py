dbc1fdcdc9462de599243256b3231b8bcdd66d0d
CharkovPA <charkov.edu@yandex.ru>
2023-05-13 14:48:45 +0600

    week2

--- solution.py
+++ solution.py
@@ -5,22 +5,12 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
 
-data = pd.read_csv('titanic.csv', usecols=['Pclass', 'Age', 'Sex', 'Fare', 'Survived'])\
-    .dropna().reset_index().replace('female', 0).replace('male', 1)
-X = data[['Pclass', 'Age', 'Sex', 'Fare']]
-Y = data[['Survived']]
-clf = DecisionTreeClassifier(random_state=241)
-clf.fit(X, Y)
-ips = clf.feature_importances_
-result = {}
-i = 0
-for key in X:
-    result[key] = ips[i]
-    i += 1
-key_max_value_01 = max(result, key=result.get)
-del result[key_max_value_01]
-key_max_value_02 = max(result, key=result.get)
-with open('result.txt', 'w', encoding="ansi") as f:
-    f.write(key_max_value_02)
-    f.writelines(' ')
-    f.writelines(key_max_value_01)
+with open("files/wine.data", "r") as file_01:
+    data = file_01.read()
+
+winedata = pd.read_csv("files/wine.data")
+print(type(winedata))
+print(winedata)
+print('----------------')
+print(winedata[0:0])
+
